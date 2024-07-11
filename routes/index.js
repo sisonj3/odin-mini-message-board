c4efebbfd2efd6ad37910ebd6e81c7874ebfc81f
sisonj <98570174+sisonj3@users.noreply.github.com>
2024-07-11 14:07:58 -0700

    Added post for displaying new messages

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,14 @@ router.get('/new', function (req, res, next) {
   res.render('form');
 });
 
+router.post('/new', function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date()
+  });
+
+  res.redirect('/');
+})
+
 module.exports = router;
